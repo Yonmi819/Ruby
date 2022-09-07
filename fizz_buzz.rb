5cb04972f7951b8595c7b753c72dafd8fe271a10
EC2 Default User <ec2-user@ip-172-31-42-12.ap-northeast-1.compute.internal>
2022-09-07 00:38:08 +0000

    [finish]Ruby1-12

---
+++ fizz_buzz.rb
@@ -0,0 +1,6 @@
+puts "数字を入力してください"
+fizz_buzz=gets
+
+def fiz_buzz()
+
+puts "入力結果は#{fizz_buzz}です"
---
+++ lesson6-1.rb
@@ -0,0 +1,9 @@
+puts "計算を始めてください"
+puts "2つの値を入力してください"
+
+a=gets.to_s
+b=gets.to_s
+
+puts "計算結果を出力します"
+puts "a * b=#{a*b}です"
+puts "計算を終了します"
