2f43725c07b4f07b4174f019a275fde66d66665c
EmiSuzuki <mush.2525miumiu@gmail.com>
2022-10-16 11:52:28 +0000

    コメント追加

--- array.rb
+++ array.rb
@@ -1,2 +1,10 @@
 names = ["Git","HTML","CSS"]
 puts names[1]
+
+wdays = ["月","火","水","木","金"]
+wdays.delete("水")
+puts wdays
+
+# 1 変数wdaysを文字列月火水木金に紐づけろ
+# 2 変数wdaysから文字列金を削除しろ
+# 3 変数wdaysを表示しろ 
--- weight.rb
+++ weight.rb
@@ -2,3 +2,6 @@ name = "A"
 weight = 50
 
 puts "#{name}さんの体重は#{weight}kgです"
+puts "#{name}さんの体重は#{weight*2}kgです"
+
+#シングルクォーテーションで囲ってしまうと文字列として認識されてしまうので注意
